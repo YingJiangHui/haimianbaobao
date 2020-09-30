a03dd3a7d7fca37d93adf281d6296d4389e66483
dark <473380917@qq.com>
2020-09-30 23:10:55 +0800

    判断hole是否位置重叠

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ const {clientWidth, clientHeight} = document.documentElement
 const holeDataList = [
     {size: 50, h: 50, w: 20},
     {size: 60, h: 60, w: 30},
+    {size: 60, h: 60, w: 30},
 ]
 
 function random(min, max) {
@@ -21,26 +22,26 @@ function random(min, max) {
 }
 
 const existPosList = []
+const xMap = new Map()
+const yMap = new Map()
+
+for (let i = 1;i<=clientWidth;i++){
+    xMap.set(i,false)
+}
+for (let i = 1;i<=clientHeight;i++){
+    yMap.set(i,false)
+}
 for (let item of holeDataList) {
     const {size, h, w, rotate} = item
     const hole = document.createElement('div')
     let [x, y] = [random(0, clientWidth - size), random(0, clientHeight - size)]
     for (let posItem of existPosList) {
         const {x: oldX, y: oldY, size: oldSize} = posItem
-        while (
-            (oldX <= x + size && x + size <= oldX + oldSize) || (oldX <= x && x <= oldX + oldSize) &&
-            (oldY <= y + size && y + size <= oldY + oldSize) || (oldY <= y && x <= oldY + oldSize)
-            ) {
-            x = random(0, clientWidth - size)
-            y = random(0, clientHeight - size)
-            console.log('old')
-
-            console.log(oldY,oldX)
-            console.log('new')
-            console.log(y,x)
-            console.log((oldX <= x + size && x + size <= oldX + oldSize), (oldX <= x && x <= oldX + oldSize))
-            console.log((oldY <= y + size && y + size <= oldY + oldSize), (oldY <= y && x <= oldY + oldSize))
+        while (y < oldY + oldSize && y + size > oldY && x < oldX + oldSize && x + size > oldX) {
+            [x, y] = [random(0, clientWidth - size), random(0, clientHeight - size)]
         }
+        console.log(y < oldY + oldSize && oldY < y + size && x < oldX + oldSize && x + size > oldX)
+
     }
     hole.classList.add('hole')
     hole.style.width = `${size}px`
@@ -51,4 +52,3 @@ for (let item of holeDataList) {
     existPosList.push({x, y, size})
     app.appendChild(hole)
 }
-console.log(existPosList)
