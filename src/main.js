e0efdd7cab4bb2ef58b847503e13150e4306b7bf
dark <473380917@qq.com>
2020-09-30 23:47:17 +0800

    使用map

--- src/main.js
+++ src/main.js
@@ -14,7 +14,6 @@ const {clientWidth, clientHeight} = document.documentElement
 const holeDataList = [
     {size: 50, h: 50, w: 20},
     {size: 60, h: 60, w: 30},
-    {size: 60, h: 60, w: 30},
 ]
 
 function random(min, max) {
@@ -25,24 +24,30 @@ const existPosList = []
 const xMap = new Map()
 const yMap = new Map()
 
-for (let i = 1;i<=clientWidth;i++){
+for (let i = 0;i<=clientWidth;i++){
     xMap.set(i,false)
 }
-for (let i = 1;i<=clientHeight;i++){
+for (let i = 0;i<=clientHeight;i++){
     yMap.set(i,false)
 }
+function fillMap(map,min,max){
+    for(let i = min;i<=max;i++){
+        map.set(i,true)
+    }
+}
+
 for (let item of holeDataList) {
     const {size, h, w, rotate} = item
     const hole = document.createElement('div')
     let [x, y] = [random(0, clientWidth - size), random(0, clientHeight - size)]
-    for (let posItem of existPosList) {
-        const {x: oldX, y: oldY, size: oldSize} = posItem
-        while (y < oldY + oldSize && y + size > oldY && x < oldX + oldSize && x + size > oldX) {
-            [x, y] = [random(0, clientWidth - size), random(0, clientHeight - size)]
-        }
-        console.log(y < oldY + oldSize && oldY < y + size && x < oldX + oldSize && x + size > oldX)
 
+    while ((yMap.get(y)||yMap.get(y+size))&&(xMap.get(x)||xMap.get(x+size))){
+        console.log('fuck')
+        x = random(0, clientWidth - size)
+        y = random(0, clientHeight - size)
     }
+    fillMap(xMap,x,x+size)
+    fillMap(yMap,y,y+size)
     hole.classList.add('hole')
     hole.style.width = `${size}px`
     hole.style.height = `${size}px`
@@ -52,3 +57,6 @@ for (let item of holeDataList) {
     existPosList.push({x, y, size})
     app.appendChild(hole)
 }
+console.log(existPosList)
+console.log(xMap)
+console.log(yMap)
