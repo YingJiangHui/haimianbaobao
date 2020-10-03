b7edd5bebc299ee571f4d43f00569ceb620446ca
dark <473380917@qq.com>
2020-10-03 15:02:00 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf dist &&
+parcel build index.html --public-url ./&&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git remote add gitee git@gitee.com:yingjianghui/haimianbaobao.git &&
+
+git push -u gitee master -f &&
+cd -
