027a22bd7542778e559e612483c2381fbc2a73a9
dark <473380917@qq.com>
2020-10-03 15:08:11 +0800

    ä¸一键部署

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ git init &&
 git add . &&
 git commit -m 'deploy' &&
 git remote add gitee git@gitee.com:yingjianghui/haimianbaobao.git &&
+git remote add github git@github.com:YingJiangHui/haimianbaobao-websit.git &&
 
 git push -u gitee master -f &&
+git push -u github master -f &&
+
 cd -
